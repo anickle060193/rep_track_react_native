5a7767c6bee97441b3aaef78f264784f8104f5a4
Adam Nickle <anickle060193@gmail.com>
2018-03-11 23:04:51 -0500

    Add basic ability to copy workouts. Sort workouts by date.

--- src/screens/Workouts/index.tsx
+++ src/screens/Workouts/index.tsx
@@ -6,8 +6,9 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import ActionButton from 'react-native-action-button';
 import { MKColor } from 'react-native-material-kit';
 import { SwipeListView, RowsMap } from 'react-native-swipe-list-view';
+import uuid from 'uuid/v4';
 
-import { removeWorkout } from '@store/reducers/workouts';
+import { removeWorkout, addWorkout } from '@store/reducers/workouts';
 
 import { Routes } from '@utils/routes';
 import { Workout, WorkoutsMap, workoutsMapToArray } from '@utils/workout';
@@ -19,6 +20,7 @@ interface PropsFromState
 
 interface PropsFromDispatch
 {
+  addWorkout: typeof addWorkout;
   removeWorkout: typeof removeWorkout;
 }
 
@@ -33,6 +35,10 @@ class Home extends React.Component<Props>
 
   render()
   {
+    let workouts = workoutsMapToArray( this.props.workouts )
+      .sort( ( a, b ) => a.date.getTime() - b.date.getTime() )
+      .reverse();
+
     return (
       <View style={styles.column}>
         <SwipeListView
@@ -43,9 +49,15 @@ class Home extends React.Component<Props>
           closeOnRowBeginSwipe={true}
           closeOnRowPress={true}
           closeOnScroll={true}
-          data={workoutsMapToArray( this.props.workouts )}
+          data={workouts}
           keyExtractor={( workout: Workout ) => workout.id}
-          renderItem={( { item: workout } ) => <WorkoutListItem workout={workout} />}
+          renderItem={( { item: workout } ) => (
+            <WorkoutListItem
+              workout={workout}
+              onPress={this.onWorkoutPress}
+              onLongPress={this.onWorkoutLongPress}
+            />
+          )}
           renderHiddenItem={( { item: workout } ) => (
             <View style={styles.workListItemSwipeBack}>
               <Icon style={styles.workoutListItemDelete} name="delete" />
@@ -73,14 +85,36 @@ class Home extends React.Component<Props>
     rows[ workoutId ].closeRow();
     this.props.removeWorkout( workoutId );
   }
+
+  private onWorkoutPress = ( workout: Workout ) =>
+  {
+  }
+
+  private onWorkoutLongPress = ( workout: Workout ) =>
+  {
+    this.props.addWorkout( {
+      ...workout,
+      id: uuid(),
+      date: new Date(),
+      name: workout.name + ' - Copy'
+    } );
+  }
 }
 
-const WorkoutListItem: React.SFC<{ workout: Workout }> = ( { workout } ) => (
-  <TouchableNativeFeedback background={TouchableNativeFeedback.SelectableBackground()}>
+const WorkoutListItem: React.SFC<{
+  workout: Workout;
+  onPress: ( workout: Workout ) => void;
+  onLongPress: ( workout: Workout ) => void;
+}> = ( { workout, onPress, onLongPress } ) => (
+  <TouchableNativeFeedback
+    background={TouchableNativeFeedback.SelectableBackground()}
+    onPress={() => onPress( workout )}
+    onLongPress={() => onLongPress( workout )}
+  >
     <View style={styles.workoutListItem}>
       <View style={styles.workoutListItemLine}>
         <Text style={styles.workoutListItemTitle}>
-          {workout.name} - {workout.date.toDateString()}
+          {workout.date.toDateString()} - {workout.name}
         </Text>
       </View>
       <View style={styles.workoutListItemLine}>
@@ -135,6 +169,7 @@ export default connect<PropsFromState, PropsFromDispatch, OwnProps, RootState>(
     workouts: state.workouts.workouts
   } ),
   {
+    addWorkout,
     removeWorkout
   }
 )( Home );
